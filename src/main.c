14029eedd41ee65e8752d750f833d5ca52af625e
BhushithaHashan <bhushithahashan99@gmail.com>
2025-06-30 09:21:43 +0530

    Add interactive CLI for link type, target directory, and link name

--- src/main.c
+++ src/main.c
@@ -5,6 +5,28 @@
 
 #define MAX_PATH 512// Max path buffer
 
+void interactive_link(const char *source) {
+    int type;
+    char dir[MAX_PATH];
+    char name[MAX_PATH];
+
+    // Ask the user for link type
+    printf("Choose link type:\n1) Soft link\n2) Hard link\n> ");
+    scanf("%d",&type);
+    getchar();// consume newline
+
+    //Ask target directory
+    printf("Enter target directory:\n> ");
+    fgets(dir, MAX_PATH, stdin);
+    dir[strcspn(dir, "\n")] = 0;
+
+    //Ask new link name
+    printf("Enter new link name:\n> ");
+    fgets(name,MAX_PATH,stdin);
+    name[strcspn(name,"\n")] = 0;
+}
+
+
 int main(int argc, char *argv[]) {
     //Check arguments
     if (argc != 3||strcmp(argv[1], "link")!=0) {
